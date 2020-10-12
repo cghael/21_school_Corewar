78ed44c309584a1268a20835077e4fa4881ec7fb
ksemele <greengrunt@gmail.com>
2020-10-12 10:54:35 +0300

    fix CMake project

---
+++ cmake-build-debug/CMakeFiles/asm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asm.dir/sources/asm.c.o"
+  "asm"
+  "asm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/asm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/corewar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/corewar.dir/sources/corewar.c.o"
+  "corewar"
+  "corewar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/corewar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
