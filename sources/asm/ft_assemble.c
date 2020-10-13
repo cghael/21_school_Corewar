3f42dd66c3c21af025ec03cc9817d891fc9047ee
cghael <a.p.gorobets@gmail.com>
2020-10-13 19:53:52 +0300

    little fix

--- sources/asm/ft_assemble.c
+++ sources/asm/ft_assemble.c
@@ -9,9 +9,8 @@ void		ft_assemble(char *file, t_asm *asm_struct)
 	ft_printf("assemble! %s\n", file);
 	//valide file
 	//if valid -> parse file -> translate to binary file:
-	//create file with same name .cor
 	ft_open_solution_file(file, asm_struct);
-//	ft_write_bytecode_to_file(asm_struct->fd, asm_struct);//todo my penis is big
+//	ft_write_bytecode_to_file(asm_struct->fd_solution, asm_struct);//todo my penis is big
 	close(asm_struct->fd);//todo err management?
 	close(asm_struct->fd_solution);//todo err management?
 }
