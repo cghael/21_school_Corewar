9b3619d5ae491a2d6670077b2965160e213724d1
esnowpea <metelkovia@yandex.ru>
2020-11-25 13:07:07 +0300

    add vm_checkout

--- sources/cw/operation.c
+++ sources/cw/operation.c
@@ -12,15 +12,67 @@
 
 #include "corewar.h"
 
+void	ft_lstpdelone(t_list **alst, t_list *del) //TODO add to libft
+{
+	t_list	*tmp;
+	if (!alst || !(*alst) || !del)
+		return ;
+	if (*alst == del)
+	{
+		*alst = (*alst)->next;
+		free(del);
+		del = 0;
+		return ;
+	}
+	else
+	{
+		tmp = *alst;
+		while (tmp->next && tmp->next != del)
+			tmp = tmp->next;
+		tmp->next = del->next;
+		free(del);
+		del = 0;
+		return ;
+	}
+}
+
+int			vm_checkout(t_vm *vm)
+{
+	t_list	*tmp;
+
+	tmp = vm->carriages;
+	while (tmp)
+	{
+		if (vm->number_cycle - ((t_carriage*)tmp->content)->number_last_live
+		<= vm->cycles_to_die || vm->cycles_to_die <= 0)
+			ft_lstpdelone(&vm->carriages, tmp);
+		tmp = tmp->next;
+	}
+	if (vm->number_live >= NBR_LIVE || vm->number_checks >= MAX_CHECKS)
+	{
+		vm->cycles_to_die -= CYCLE_DELTA;
+		vm->number_checks = 0;
+	}
+	if (vm->carriages)
+		return (1);
+	else
+		return (0);
+}
+
 t_player	*vm_operation(t_vm *vm)
 {
-	while (checkout())
-	{
+	unsigned int	current;
 
+	while (vm_checkout(vm))
+	{
+		current = 0;
+		while (current < vm->cycles_to_die)
+		{
+			vm_survey_carriages(vm);
+			current++;
+		}
+		vm->number_cycle += current;
+		vm->number_checks++;
 	}
-	//TODO carriage list processing
-	//TODO cycle counting
-	//TODO processing checks
-	//TODO cycle_to_die change
 	return (vm->last_live_player);
 }
