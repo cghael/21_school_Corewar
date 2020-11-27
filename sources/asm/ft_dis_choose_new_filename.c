c5c0d1d201d355fcf6325eb8f243620881e80af3
ksemele <greengrunt@gmail.com>
2020-11-27 12:42:47 +0300

    fix norm to _dis_ funcs

--- sources/asm/ft_dis_choose_new_filename.c
+++ sources/asm/ft_dis_choose_new_filename.c
@@ -36,13 +36,13 @@ int				ft_dis_choose_new_filename(t_dis *dis_s)
 	char		*answer;
 	char		*tmp;
 
-//	tmp = ft_strdup(FILE_EXIST_TXT FILE_INPUT_NEW);//todo hmm?
 	answer = ft_strnew(0);
 	if (EXIT_FAILURE == ft_init_strings(answer, dis_s))
 		return (EXIT_FAILURE);
 	while (FILE_EXIST == ft_dis_check_file_exist(dis_s) || dis_s->fd_cor < 0)
 	{
-		ft_printf(FILE_EXIST_TXT FILE_INPUT_NEW, dis_s->file_s);
+		ft_printf(FILE_EXIST_TXT, dis_s->file_s);
+		ft_printf(FILE_INPUT_NEW);
 		ft_dis_ask_new_filename(dis_s);
 		if (FILE_NOT_EXIST == ft_dis_check_file_exist(dis_s))
 		{
