9583ba202dfa6bde62cb6a43622123198f6f6857
esnowpea <vaanyaa97@mail.ru>
2021-02-04 12:44:26 +0300

    fix st

--- sources/commands/st.c
+++ sources/commands/st.c
@@ -17,12 +17,12 @@ void	st(t_carriage *car, t_vm *vm)
 	int		i;
 
 	(void)vm;
+	set_array(car->args[1].data, car->args[0].data, REG_SIZE);
 	i = 0;
 	while (i < REG_SIZE)
 	{
-		car->args[0].data.data[(car->args[0].pos + i) %
-		car->args[0].data.max].num_player =	car->player->number;
+		car->args[1].data.data[(car->args[1].data.pos + i) %
+		car->args[1].data.max].num_player =	car->player->number;
 		i++;
 	}
-	set_array(car->args[1].data, car->args[0].data, REG_SIZE);
 }
--- sources/commands/sti.c
+++ sources/commands/sti.c
@@ -15,15 +15,9 @@
 void	sti(t_carriage *car, t_vm *vm)
 {
 	int32_t		ind;
+	t_data		data;
 	int 		i;
 
-	i = 0;
-	while (i < REG_SIZE)
-	{
-		car->args[0].data.data[(car->args[0].pos + i) %
-		car->args[0].data.max].num_player =	car->player->number;
-		i++;
-	}
 	if (car->args[1].type == T_DIR)
 		ind = (int16_t)ft_bytetoint(car->args[1].data, IND_SIZE);
 	else
@@ -33,6 +27,12 @@ void	sti(t_carriage *car, t_vm *vm)
 	else
 		ind += ft_bytetoint(car->args[2].data, REG_SIZE);
 	ind %= IDX_MOD;
-	set_array(get_t_data(vm->arena, car->position + ind,
-		MEM_SIZE), car->args[0].data, REG_SIZE);
+	data = get_t_data(vm->arena, car->position + ind, MEM_SIZE);
+	set_array(data, car->args[0].data, REG_SIZE);
+	i = 0;
+	while (i < REG_SIZE)
+	{
+		data.data[(data.pos + i) % data.max].num_player = car->player->number;
+		i++;
+	}
 }
